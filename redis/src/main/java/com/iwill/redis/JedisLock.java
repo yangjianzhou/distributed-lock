6cfd7c0697c60a27064594bd88e24b090205d4b1
iwill <yangjianzhouctgu@126.com>
2018-08-14 10:58:55 +0800

    add code to jedis

--- redis/src/main/java/com/iwill/redis/JedisLock.java
+++ redis/src/main/java/com/iwill/redis/JedisLock.java
@@ -6,6 +6,10 @@ public class JedisLock {
 
     public static void main(String[] args) throws Exception{
         Jedis jedis = new Jedis("redis://127.0.0.1:6379");
-        jedis.setex("name",10,"yangjianzhou");
+        long locked = jedis.setnx("name","yangjianzhou");
+        System.out.println(locked);
+        String script = "return redis.pcall('setnx',KEYS[1],'bar')";
+        Object object = jedis.eval(script , 1 ,"bar");
+        System.out.println(object);
     }
 }
